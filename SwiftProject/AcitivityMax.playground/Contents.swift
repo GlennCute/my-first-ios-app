87f5b7e3910d1bed48a8c1e05d141c8bf540dbd0
Glenn Diaz Jr <diazjrglenngozum@gmail.com>
2022-01-07 13:27:36 +0800

    FinalMax

--- SwiftProject/AcitivityMax.playground/Contents.swift
+++ SwiftProject/AcitivityMax.playground/Contents.swift
@@ -1,11 +1,16 @@
 import UIKit
 import Darwin
 
+
 func getOddNumbers(withMax max: Int) -> Array<Int> {
+    
     return Array(0...max).compactMap { $0 % 2 != 0 ? $0 : nil }
+    
         }
     
-    let max: Int = 150
+let max: Int = 150
+
 let oddNumbers = getOddNumbers(withMax: max)
+
 print("Array of Oddnumbers from 0 to \(max) are : \(oddNumbers)")
 
